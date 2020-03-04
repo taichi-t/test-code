5b2b6311ed5977d97469cfe34f424e3bf2591d6a
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-03-04 12:00:00 +0900

    added:file

---
+++ component&arrowFunctions.jsx
@@ -0,0 +1,18 @@
+NG;
+// render () {
+//     return <Child change={() => console.log('hoge')} />
+//   }
+
+OK;
+// constructor(props) {
+//     super(props)
+//     this.hoge = this.hoge.bind(this)
+//   }
+
+//   hoge() {
+//     console.log('hoge')
+//   }
+
+//   render () {
+//     return <Child change={this.hoge} />
+//   }
